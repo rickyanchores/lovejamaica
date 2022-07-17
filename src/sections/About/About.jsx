0efcf1ed9feedd688d52a8b7e8166303395e7da2
RickyAnchores <ricky23anchores@gmail.com>
2022-07-17 18:10:17 +0100

    edit v3

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,7 +5,10 @@ const About = () => {
     <div className='About' id='about'>
         <div className="container">
           <h1>About</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis rerum modi et nobis vel unde?</p>
+          <p>The company Love Jamaica was found and created in 2018, as a Jamaican
+        clothes line that specialises in creating authentic clothes for all.
+        When a product is purchased we give back to children in need across the
+        world.</p>
         </div>
     </div>
   )
--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import SocialMedia from '../../components/SocialMedia/SocialMedia';
 
 const Footer = () => {
+
+  /*
+  const linkList = [
+    {
+      name: "Instagram",
+      href: "https://www.google.co.uk/"
+    },
+    {
+      name: "Facebook",
+      href: "https://www.google.co.uk/"
+    },
+    {
+      name: "ETSY",
+      href: "https://www.google.co.uk/"
+    }
+  ];
+
+  */
+
   return (
     <div className='Footer'>
       <div className="container">
-        <h3>Footer</h3>
+        <h3>LoveJamaicaShop</h3>
+        <SocialMedia />
       </div>
     </div>
   )
